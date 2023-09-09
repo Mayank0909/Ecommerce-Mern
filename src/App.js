e9b0fde7856408fa43ba9011bd66e568232e8559
Mayank0907 <mayank.alphaware@gmail.com>
2023-09-09 07:21:56 +0530

    ui product details,checkout,cart etc

--- src/App.js
+++ src/App.js
@@ -2,13 +2,52 @@ import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
-import { ProductList } from "./features/product-list/ProductList";
+import { ProductList } from "./features/product-list/components/ProductList";
 import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
+import {
+	createBrowserRouter,
+	RouterProvider,
+	Route,
+	Link,
+} from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import { Cart } from "./features/cart/Cart";
+import Cartpage from "./pages/Cartpage";
+import Checkout from "./pages/Checkout";
+import ProductDetails from "./features/product-list/components/ProductDetails";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Homepage />,
+	},
+	{
+		path: "login",
+		element: <LoginPage />,
+	},
+	{
+		path: "signup",
+		element: <SignupPage />,
+	},
+	{
+		path: "cart",
+		element: <Cartpage />,
+	},
+	{
+		path: "Checkout",
+		element: <Checkout />,
+	},
+	{
+		path: "productinfo",
+		element: <ProductDetailPage />,
+	},
+]);
 function App() {
 	return (
 		<div className="App">
-			<Homepage />
+			<RouterProvider router={router} />
 		</div>
 	);
 }
